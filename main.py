cf6e004cddd903b48374cbfa172c0e5aa4dfb213
JonksPanda <jionte@gmail.com>
2023-11-07 14:37:30 +0100

    fixed main.py to get 10/10 from pylint

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import menu
+"""runs the project."""
+
 import os
+import menu
+
 
 def main():
+    """Inits the project."""
     #Checks if neccesary paths exists before starting the game
     data = f"{os.path.dirname(os.path.realpath(__file__))}/data"
     fleets = f"{os.path.dirname(os.path.realpath(__file__))}/data/fleets"
-    if os.path.exists(data) == False:
+    if os.path.exists(data) is False:
         os.makedirs(data)
-    if os.path.exists(fleets) == False:
+    if os.path.exists(fleets) is False:
         os.makedirs(fleets)
     menu.main()
 
-
 if __name__ == "__main__":
     main()
+    
